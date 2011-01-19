f9a5df96e75955c753f9169ffad5afcb88ffc1a8
Kyle Mathews <mathews.kyle@gmail.com>
2011-01-19 10:40:07 -0800

    Refactored code to add a centralized system for fetching defaults. Now other modules can add default variables which will be added to all tracking/funnel calls in JS or PHP.

--- mixpanel.js
+++ mixpanel.js
@@ -1,21 +1,10 @@
+// Do some initial setup.
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
-    // How often do people click on the your-groups drop down?
-    $("#your-groups").bind('click', function(event) {
-      mpq.push(["track", "click-your-groups", {"uid": Drupal.settings.user.uid, "activePath": Drupal.settings.activePath, "cohort": Drupal.settings.user.cohort, "distinct_id": Drupal.settings.user.uid}]);
-    });
-    // How often do people click on the my-groups drop down?
-    $("#session").bind('click', function(event) {
-      mpq.push(["track", "click-session-dropdown", {"uid": Drupal.settings.user.uid, "activePath": Drupal.settings.activePath, "cohort": Drupal.settings.user.cohort, "distinct_id": Drupal.settings.user.uid}]);
-    });
-    // How often do people click "expand this post"?
-    $(".activity-teaser-expand").bind('click', function(event) {
-      mpq.push(["track", "click-expand-this-post", {"uid": Drupal.settings.user.uid, "cohort": Drupal.settings.user.cohort, "distinct_id": Drupal.settings.user.uid}]);
-      return false;
-    });
-    // How often do people click on the group settings tab?
-    $("#group-settings").bind('click', function(event) {
-      mpq.push(["track", "click-group-settings", {"uid": Drupal.settings.user.uid, "cohort": Drupal.settings.user.cohort, "distinct_id": Drupal.settings.user.uid}]);
-    });
+    // Uniquely identify the user;
+    mpq.push(["identify", Drupal.settings.user.uid]);
+
+    // Register properties about the user.
+    mpq.push(["register", Drupal.settings.mixpanel.defaults]);
   });
 }
