ce2a83cfccf7b797cb80033d7b66a3e2c1c5a4f2
Kyle Mathews <mathews.kyle@gmail.com>
2011-01-20 16:06:59 -0800

    Used old settings var.

--- mixpanel.js
+++ mixpanel.js
@@ -2,7 +2,7 @@
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
     // Uniquely identify the user;
-    mpq.push(["identify", Drupal.settings.user.uid]);
+    mpq.push(["identify", Drupal.settings.mixpanel.defaults.uid]);
 
     // Register properties about the user.
     mpq.push(["register", Drupal.settings.mixpanel.defaults]);
